67fdc42d324a8f72a07acabe364e5ab72f13ae78
Amira Essaadaoui <22400940@me.uvsq.fr>
2026-02-20 11:13:48 +0100

    Ajout de la soustraction, la multiplication et la division

--- pycalc.py
+++ pycalc.py
@@ -1,4 +1,10 @@
-def addition (elt, elt1):
-    print(elt + elt1)
-
-addition(2,2)
+def addition (elt, operande, elt1):
+    operande = ["+", "-", "*", "/"]
+    if operande == "+":
+        print(elt + elt1)
+    elif operande == "-":
+        print(elt - elt1)
+    elif operande == "*":
+        print(elt * elt1)
+    else:
+        print(elt / elt1)
